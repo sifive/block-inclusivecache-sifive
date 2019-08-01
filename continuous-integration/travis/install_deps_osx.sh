2b7c5e4ec718bef035bcd59af4bdcb257850364c
Abert Chen <albert.chen@sifive.com>
2019-08-01 09:03:34 -0700

    get rid of environment-example-sifive, add -dv

--- continuous-integration/travis/install_deps_osx.sh
+++ continuous-integration/travis/install_deps_osx.sh
@@ -2,7 +2,6 @@
 
 echo "Installing Wake"
 
-export USE_FUSE_WAKE=0
 git clone https://github.com/sifive/wake.git
 git -C wake checkout v0.15.1
 make -C wake
--- continuous-integration/travis/test.sh
+++ continuous-integration/travis/test.sh
@@ -9,11 +9,8 @@ echo "Initialize Workspace"
 git config --global url."https://github.com/".insteadOf 'git@github.com:'
 wit --repo-path $PWD/.. init workspace -a block-inclusivecache-sifive
 cd workspace/
-wit add-pkg https://github.com/sifive/environment-example-sifive.git
-wit update
-
-wake --init .
 
 echo "Compile Scala"
 
-wake --no-tty -j1 -v 'compileScalaModule inclusiveCacheScalaModule | getPathResult'
+wake --init .
+wake --no-tty -j1 -dv 'compileScalaModule inclusiveCacheScalaModule | getPathResult'
