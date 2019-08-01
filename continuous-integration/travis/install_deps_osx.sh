7ffa30c2bcc0e157da201bca97fcf2241b32d01e
Abert Chen <albert.chen@sifive.com>
2019-08-01 10:04:47 -0700

    add osxfuse cask

--- continuous-integration/travis/install_deps_osx.sh
+++ continuous-integration/travis/install_deps_osx.sh
@@ -3,10 +3,10 @@
 echo "Installing Wake"
 
 git clone https://github.com/sifive/wake.git
-cd wake
-git checkout v0.15.1
-make && ./bin/wake 'install "/usr/local"'
-cd ..
+git -C wake checkout v0.15.1
+make -C wake
+export PATH=$PATH:$PWD/wake/bin
+
 
 echo "Installing Wit"
 
