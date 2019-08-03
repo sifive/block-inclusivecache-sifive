689622302a35fa9f3a2017d612b659181cbb20a1
Abert Chen <albert.chen@sifive.com>
2019-08-02 18:30:08 -0700

    checkout exact wake commit

--- continuous-integration/travis/install_deps_osx.sh
+++ continuous-integration/travis/install_deps_osx.sh
@@ -2,7 +2,8 @@
 
 echo "Installing Wake"
 
-git clone --branch old-osx https://github.com/sifive/wake.git
+git clone https://github.com/sifive/wake.git
+git -C wake checkout a539d6700afd02dd0f83e4c887f98650e7bdebb1 # commit of mac os fix
 make -C wake
 export PATH=$PATH:$PWD/wake/bin
 
